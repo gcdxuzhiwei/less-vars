1c0bd6cb281973db514fd9051fb83a4413c2d70c
xuzhiwei <xuzhiwei@qtrade.com.cn>
2021-07-05 17:14:51 +0800

    feat: use less-vars-to-js

--- src/hover.js
+++ src/hover.js
@@ -1,6 +1,6 @@
 const vscode = require("vscode");
-const path = require("path");
 const fs = require("fs");
+const lessToJs = require("less-vars-to-js");
 
 /**
  * 鼠标悬停提示，当鼠标停在package.json的dependencies或者devDependencies时，
@@ -10,7 +10,7 @@ const fs = require("fs");
  * @param {*} token
  */
 async function provideHover(document, position, token) {
-  // 当前字符
+  // 查询字符
   const word = document.getText(document.getWordRangeAtPosition(position));
   // 文件路径
   const locations = vscode.workspace
@@ -22,56 +22,64 @@ async function provideHover(document, position, token) {
     return;
   }
 
-  //未配置文件路径
-  if (!locations) {
-    return new vscode.Hover("请先在settings.json中配置变量文件路径");
+  // 未配置文件路径
+  if (!locations || (locations instanceof Array && locations.length === 0)) {
+    return new vscode.Hover(
+      "请先在settings.json中配置变量文件路径,配置项为lessVars.locations"
+    );
   }
 
-  // 字符串路径传数组
+  // 字符串路径转数组
   const allFile = typeof locations === "string" ? [locations] : locations;
 
+  // 检测路径配置是否正确
+  const error = [];
+  for (let i = 0; i < allFile.length; i++) {
+    if (allFile[i].slice(-4) !== "less") {
+      error.push(`只支持less文件,${allFile[i]}`);
+    }
+    if (!fs.existsSync(allFile[i])) {
+      error.push(`路径配置有误,${allFile[i]}`);
+    }
+  }
+  if (error.length) {
+    return new vscode.Hover(error);
+  }
+
+  // 汇总所有变量
+  let allVars = {};
+  for (let i = 0; i < allFile.length; i++) {
+    const context = fs.readFileSync(allFile[i], "utf-8");
+    allVars = {
+      ...lessToJs(context, { resolveVariables: true }),
+      ...allVars,
+    };
+  }
+
+  // 开始查找，值中有变量就继续查找
   let currentWord = word;
   const hoverRes = [];
 
-  function findWord() {
-    for (let i = 0; i < allFile.length; i++) {
-      if (!fs.existsSync(allFile[i])) {
-        return new vscode.Hover(`此路径配置错误:${allFile[i]}`);
-      }
-      const context = fs.readFileSync(allFile[i], "utf-8");
-      const startIndex = context.search(currentWord);
-      if (startIndex >= 0) {
-        let endIndex = startIndex + 1;
-        while (endIndex < context.length && context[endIndex] !== ";") {
-          endIndex++;
-        }
-        const text = context.slice(startIndex, endIndex + 1);
-        hoverRes.push(text);
-        const nextText = text.slice(1, text.length);
-        const nextStart = nextText.search("@");
-        if (nextStart === -1) {
-          currentWord = "";
-        } else {
-          let nextEnd = nextStart + 1;
-          while (
-            nextEnd < nextText.length &&
-            ![";", " ", ":", ","].includes(nextText[nextEnd])
-          ) {
-            nextEnd++;
-          }
-          currentWord = nextText.slice(nextStart, nextEnd);
-        }
-        break;
-      }
-
-      if (i === allFile.length - 1) {
+  while (currentWord) {
+    const currentValue = allVars[currentWord];
+    if (!currentValue) {
+      currentWord = "";
+    } else {
+      hoverRes.push(`${currentWord} : ${currentValue} ;`);
+      const nextStart = currentValue.search("@");
+      if (nextStart < 0) {
         currentWord = "";
+      } else {
+        let nextEnd = nextStart + 1;
+        while (
+          nextEnd <= currentValue.length &&
+          /^[A-Za-z0-9-_]+$/.test(currentValue[nextEnd])
+        ) {
+          nextEnd++;
+        }
+        currentWord = currentValue.slice(nextStart, nextEnd);
       }
     }
-  }
-
-  while (currentWord) {
-    findWord();
   }
 
   return new vscode.Hover(hoverRes.length ? hoverRes : "未找到变量值");
